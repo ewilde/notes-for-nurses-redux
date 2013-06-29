a3ae2a68ca5470e7c94f4e4fb1317567220060b4
ewilde <ewilde@gmail.com>
2013-06-29 17:16:16 +0100

    Ordering of name entity by first name

---
+++ src/core.net.unittests/Model/NameTests.cs
@@ -0,0 +1,53 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="NameTests.cs" company="UBS AG">
+// Copyright (c) 2013.
+// </copyright>
+// -----------------------------------------------------------------------
+
+
+using System;
+
+using Machine.Fakes;
+using Machine.Specifications;
+
+namespace core.net.tests.Model
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Edward.Wilde.Note.For.Nurses.Core.Model;
+    
+    [Subject(typeof(Name), "Comparing")]
+    public class When_comparing_two_items_with_a_different_first_name : WithSubjectAndResult<IList<Name>, int>
+    {
+        Establish context = () =>
+            {
+                Subject = new List<Name>
+                    {
+                        new Name { FirstName = "Sarah", LastName = "Moon" },
+                        new Name { FirstName = "Adam", LastName = "Ant" },
+                    };
+            };
+
+        Because of = () => Result = Subject.ElementAt(0).CompareTo(Subject.ElementAt(1));
+
+        It should_return_more_than_zero_for_a_name_alphabetically_after_the_item_being_compared = () => Result.ShouldEqual((int)CompareResult.MoreThan);
+    }
+
+    [Subject(typeof(Name), "Comparing")]
+    public class When_comparing_two_items_with_a_equal_first_names_and_different_lastnames : WithSubjectAndResult<IList<Name>, int>
+    {
+        Establish context = () =>
+            {
+                Subject = new List<Name>
+                    {
+                        new Name { FirstName = "Adam", LastName = "Moon" },
+                        new Name { FirstName = "Adam", LastName = "Ant" },
+                    };
+            };
+
+        Because of = () => Result = Subject.ElementAt(0).CompareTo(Subject.ElementAt(1));
+
+        It should_return_more_than_zero_for_a_name_alphabetically_after_the_item_being_compared = () => Result.ShouldEqual((int)CompareResult.MoreThan);
+    }
+}
---
+++ src/core/Model/CompareResult.cs
@@ -0,0 +1,19 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="CompareResult.cs" company="UBS AG">
+// Copyright (c) 2013.
+// </copyright>
+// -----------------------------------------------------------------------
+namespace Edward.Wilde.Note.For.Nurses.Core.Model
+{
+    using System;
+
+    /// <summary>
+    /// A list of possible outcomes whilst comparing two entities of the same type <see cref="IComparable{T}"/>.
+    /// </summary>
+    public enum CompareResult
+    {
+        LessThan = -1,
+        Equal = 0,
+        MoreThan = 1
+    }
+}
